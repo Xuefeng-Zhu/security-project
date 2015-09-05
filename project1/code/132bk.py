e73c4e910518ec4fea52523b6ac421448aebef7e
Bob King <kingbob2015@hotmail.com>
2015-09-05 00:06:08 -0500

    Collision found

--- project1/code/132bk.py
+++ project1/code/132bk.py
@@ -1,5 +1,9 @@
+from utils import read_file
+import string
+import random
+
 def wha(inStr):
-    binArray = bytearray(inStr)
+    binArray = inStr
     
     hexdata1 = int('CC',16)
     hexdata2 = int('33',16)
@@ -15,5 +19,24 @@ def wha(inStr):
     return hex(outHash)
 
 if __name__=='__main__':
-    print (wha("Hello world!"))
-    print (wha("I am Groot."))
+    #print (wha(bytearray("Hello world!")))
+    #print (wha(bytearray("I am Groot.")))
+
+    jeopardyQ = read_file("../1.3.2_input_string.txt")
+    jeopardyHash = wha(bytearray(jeopardyQ))
+
+    print jeopardyHash
+
+    test = ""
+    testHash = wha(bytearray(test))
+    while (testHash!=jeopardyHash):
+        test = " PRESIDENTS NATIONAL HISTORIC SITE IN WEST BRANCH IOWA HAS A QUAKER MEETINGHOUSE ON THE GROUNDS"
+        for i in range (0,4):
+            test = random.choice(string.ascii_uppercase) + test
+        testHash = wha(bytearray(test))
+        
+
+    print test, testHash
+        
+
+    
